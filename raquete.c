db1a281abd807e90c13c0b73d320882373090129
Paulo Bardes <paulobardes@ig.com.br>
2013-05-28 14:00:26 -0300

    Implementando desenhaRaquete()

--- raquete.c
+++ raquete.c
@@ -1,1 +1,13 @@
 #include "raquete.h"
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+
+void desenhaRaquete(const BN_RAQUETE *raq)
+{
+    /* Desenhado o retângulo da raquete */
+    al_draw_filled_rectangle(raq->x - (raq->larg / 2), raq->y - (raq->alt / 2),
+                             raq->x + (raq->larg / 2), raq->y + (raq->alt / 2),
+                             raq->cor);
+    /* TODO Desenhar "efeitos especiais". Ex: sombra, campo de força etc.. */
+}
--- raquete.h
+++ raquete.h
@@ -1,5 +1,18 @@
 #ifndef RAQUETE_H
 #define RAQUETE_H
 
+#include <allegro5/allegro_color.h>
+
+/* Struct para controlar a raquete */
+struct bn_raquete_t
+{
+    float x, y;         /* Centro da raquete. */
+    float larg, alt;    /* Largura e altura da raquete. */
+    ALLEGRO_COLOR cor;  /* Cor da raquete. */
+};
+typedef struct bn_raquete_t BN_RAQUETE;
+
+/* Desenha a raquete no "target" atual */
+void desenhaRaquete(const BN_RAQUETE *raq);
 
 #endif
